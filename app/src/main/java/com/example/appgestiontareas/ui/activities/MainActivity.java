1dd96e8886ba638619b92a19ca32a996a1977720
Pablo RG <pabloromo169@gmail.com>
2025-11-20 12:47:15 +0100

    Añadidos 3 fragments para la BottomNavigationTool

---
+++ app/src/main/java/com/example/appgestiontareas/ui/activities/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.appgestiontareas.ui.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.example.appgestiontareas.R;
+import com.example.appgestiontareas.ui.fragments.CalendarFragment;
+import com.example.appgestiontareas.ui.fragments.HomeFragment;
+import com.example.appgestiontareas.ui.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Ocultar la barra de estado y la barra de navegación
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // Ocultar la ActionBar si existe
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setSelectedItemId(R.id.nav_home);
+
+
+        // Mostrar fragment inicial
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        // Listener para cambio de pantallas
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            Fragment selected = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selected = new HomeFragment();
+            } else if (itemId == R.id.nav_settings) {
+                selected = new SettingsFragment();
+            } else if (itemId == R.id.nav_calendar) {
+                selected = new CalendarFragment();
+            }
+
+            if (selected != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selected)
+                        .commit();
+            }
+
+            return true;
+        });
+    }
+}
