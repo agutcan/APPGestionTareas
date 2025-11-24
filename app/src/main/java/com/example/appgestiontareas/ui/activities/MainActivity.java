dfe71071e66b7503888a0d6eb93064274e720a39
Pablo RG <pabloromo169@gmail.com>
2025-11-24 09:10:04 +0100

    Creado LoginActivity y cambiado los Fragments a ConstraintLayout

--- app/src/main/java/com/example/appgestiontareas/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/appgestiontareas/ui/activities/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.appgestiontareas.ui.activities;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.appgestiontareas.R;
@@ -18,7 +12,6 @@ import com.example.appgestiontareas.ui.fragments.CalendarFragment;
 import com.example.appgestiontareas.ui.fragments.HomeFragment;
 import com.example.appgestiontareas.ui.fragments.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
