0f1bab26cd9bb25dfc186b9d0e3b237b1eafea82
Pablo RG <pabloromo169@gmail.com>
2025-11-26 11:50:40 +0100

    RecyclerView añadido al HomeFragment y su respectivo CardAdapter

--- app/src/main/java/com/example/appgestiontareas/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/appgestiontareas/ui/fragments/HomeFragment.java
@@ -8,8 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appgestiontareas.R;
+import com.example.appgestiontareas.ui.adapters.CardAdapter;
+
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
 
@@ -21,6 +27,30 @@ public class HomeFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rv = view.findViewById(R.id.rvCards);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        ArrayList<String> tarjetas = new ArrayList<>();
+        tarjetas.add("Actividad 1");
+        tarjetas.add("Actividad 2");
+        tarjetas.add("Actividad 3");
+        tarjetas.add("Actividad 4");
+        tarjetas.add("Actividad 5");
+        tarjetas.add("Actividad 6");
+        tarjetas.add("Actividad 7");
+        tarjetas.add("Actividad 8");
+        tarjetas.add("Actividad 9");
+        tarjetas.add("Actividad 10");
+        // añadir tantas como quieras dinámicamente
+
+        CardAdapter adapter = new CardAdapter(tarjetas);
+        rv.setAdapter(adapter);
     }
 }
 
