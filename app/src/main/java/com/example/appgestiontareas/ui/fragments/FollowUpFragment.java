f16b5f58eb8285cfed74fe7d9f1ee3f014c37b40
Antonio1012 <antonioloeches9@gmail.com>
2025-12-07 12:07:03 +0100

    mitad de fragment horario

--- app/src/main/java/com/example/appgestiontareas/ui/fragments/FollowUpFragment.java
+++ app/src/main/java/com/example/appgestiontareas/ui/fragments/FollowUpFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +22,19 @@ public class FollowUpFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_follow_up, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        Button btnHorario = view.findViewById(R.id.btnHorario);
+        btnHorario.setOnClickListener(v -> {
+            Fragment nuevoFragment = new ScheduleFragment();
+            getParentFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, nuevoFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 }
 
--- app/src/main/java/com/example/appgestiontareas/ui/fragments/ScheduleFragment.java
+++ app/src/main/java/com/example/appgestiontareas/ui/fragments/ScheduleFragment.java
@@ -4,21 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appgestiontareas.R;
-import com.example.appgestiontareas.ui.database.repository.ActividadRepository;
+import com.example.appgestiontareas.ui.HorarioAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ScheduleFragment extends Fragment {
+
+    Button btnLunes, btnMartes, btnMiercoles, btnJueves, btnViernes;
+    RecyclerView recyclerAsignaturas;
+
+    HashMap<String, ArrayList<String>> horario = new HashMap<>();
+    HorarioAdapter adapter;
 
     public ScheduleFragment() {}
 
@@ -33,5 +40,75 @@ public class ScheduleFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+        btnLunes = view.findViewById(R.id.btnLunes);
+        btnMartes = view.findViewById(R.id.btnMartes);
+        btnMiercoles = view.findViewById(R.id.btnMiercoles);
+        btnJueves = view.findViewById(R.id.btnJueves);
+        btnViernes = view.findViewById(R.id.btnViernes);
+
+        recyclerAsignaturas = view.findViewById(R.id.recyclerAsignaturas);
+        recyclerAsignaturas.setLayoutManager(new LinearLayoutManager(requireContext()));
+        cargarDatosHorario();
+
+        adapter = new HorarioAdapter(new ArrayList<>(horario.get("Lunes")));
+        recyclerAsignaturas.setAdapter(adapter);
+
+        btnLunes.setOnClickListener(v -> cargarDia("Lunes"));
+        btnMartes.setOnClickListener(v -> cargarDia("Martes"));
+        btnMiercoles.setOnClickListener(v -> cargarDia("Miércoles"));
+        btnJueves.setOnClickListener(v -> cargarDia("Jueves"));
+        btnViernes.setOnClickListener(v -> cargarDia("Viernes"));
+
+
     }
+
+    private void cargarDatosHorario() {
+        horario.put("Lunes", new ArrayList<String>() {{
+            add("Lengua");
+            add("Matemáticas");
+            add("Conocimiento del Medio");
+            add("Inglés");
+            add("Refuerzo/Lectura");
+        }});
+
+        horario.put("Martes", new ArrayList<String>() {{
+            add("Lengua");
+            add("Matemáticas");
+            add("Francés");
+            add("Educación Física");
+            add("Refuerzo/Lectura");
+        }});
+
+        horario.put("Miércoles", new ArrayList<String>() {{
+            add("Inglés");
+            add("Lengua");
+            add("Música");
+            add("Matemáticas");
+            add("Conocimiento del Medio");
+        }});
+
+        horario.put("Jueves", new ArrayList<String>() {{
+            add("Matemáticas");
+            add("Lengua");
+            add("Educación Física");
+            add("Inglés");
+            add("Francés");
+        }});
+
+        horario.put("Viernes", new ArrayList<String>() {{
+            add("Religión");
+            add("Lengua");
+            add("Matemáticas");
+            add("Refuerzo/Lectura");
+            add("Conocimiento del Medio");
+        }});
+    }
+
+    private void cargarDia(String dia) {
+        adapter.updateData(horario.get(dia));
+    }
+
+
+
+
 }
